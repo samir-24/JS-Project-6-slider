6a6037a56ca5d3b35eea16bef0784f3c6180ed28
SAMIR24 <samirpatel2475@gmail.com>
2025-11-19 09:52:17 +0530

    Add files via upload

---
+++ Project-6-slider-js/slider.js
@@ -0,0 +1,87 @@
+// All slides saved here
+let gallery = [
+  { image: "./img/img1.jpg", caption: "Beautiful Sunset" },
+  { image: "./img/img2.jpg", caption: "Mountain View" },
+  { image: "./img/img3.jpg", caption: "Calm Lake" }
+];
+
+let indexNumber = 0;
+let timer = null;
+
+// Show slide
+function showSlide(num) {
+  document.getElementById("slideImage").src = gallery[num].image;
+  document.getElementById("caption").textContent = "Caption: " + gallery[num].caption;
+  document.getElementById("counter").textContent = `Slide ${num + 1} of ${gallery.length}`;
+}
+
+// Next
+function nextSlide() {
+  const msg = document.getElementById("message");
+
+  if (indexNumber === gallery.length - 1) {
+    msg.textContent = "This is the last slide";
+    return;
+  }
+
+  indexNumber++;
+  msg.textContent = "";
+  showSlide(indexNumber);
+}
+
+// Previous
+function prevSlide() {
+  const msg = document.getElementById("message");
+
+  if (indexNumber === 0) {
+    msg.textContent = "This is the first slide";
+    return;
+  }
+
+  indexNumber--;
+  msg.textContent = "";
+  showSlide(indexNumber);
+}
+
+// Add new slide
+function addImage() {
+  let url = document.getElementById("imgURL").value;
+  let text = document.getElementById("imgCaption").value;
+
+  if (url === "" || text === "") {
+    alert("Please enter both Image URL and Caption");
+    return;
+  }
+
+  gallery.push({ image: url, caption: text });
+
+  indexNumber = gallery.length - 1;
+  showSlide(indexNumber);
+
+  document.getElementById("imgURL").value = "";
+  document.getElementById("imgCaption").value = "";
+}
+
+// Auto-play start
+function startAutoPlay() {
+  stopAutoPlay();
+
+  timer = setInterval(() => {
+    if (indexNumber === gallery.length - 1) {
+      document.getElementById("message").textContent = "This is the last slide";
+      return;
+    }
+    nextSlide();
+  }, 3000);
+
+  document.getElementById("message").textContent = "Auto-play started";
+}
+
+// Auto-play stop
+function stopAutoPlay() {
+  clearInterval(timer);
+  document.getElementById("message").textContent = "Auto-play stopped";
+}
+
+// Load first slide
+showSlide(indexNumber);
